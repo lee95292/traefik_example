704dbb2e2112fd0b4cfd3dce9fbe6d32a4b2fd75
Myeonggyu-Lee <lee95292@naver.com>
2021-04-03 05:47:50 +0000

    update ssl

---
+++ aliasfile.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+alias dcr='sudo docker container restart'
+alias dcl='sudo docker container logs'
+alias dex='sudo docker exec -it'
+
+alias dcr_trf='dcr traefik_traefik-gateway_1'
+alias dcl_trk='dcl traefik_traefik-gateway_1'
+alias dex_trf='dex traefik_traefik-gateway_1'
