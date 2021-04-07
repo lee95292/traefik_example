e6b839099e1abca9ad2e428d8d27d527e04e06e5
Myeonggyu-Lee <lee95292@naver.com>
2021-04-07 11:08:30 +0000

    route(vscode, traefik):  - vscode.mglee.dev  - traefik.mglee.dev
    error(tls):
     - tls timeout

--- aliasfile.sh
+++ aliasfile.sh
@@ -2,8 +2,9 @@
 
 alias dcr='sudo docker container restart'
 alias dcl='sudo docker container logs'
-alias dex='sudo docker exec -it'
 
-alias dcr_trf='dcr traefik_traefik-gateway_1'
-alias dcl_trk='dcl traefik_traefik-gateway_1'
-alias dex_trf='dex traefik_traefik-gateway_1'
+alias restart_trf='dcr traefik_traefik-gateway_1'
+alias log_trf='dcl traefik_traefik-gateway_1'
+alias exec_trf='sudo docker exec -it traefik_traefik-gateway_1 /bin/sh' 
+alias stop_trf='sudo docker stop traefik_traefik-gateway_1'
+alias start_trf='sudo docker start traefik_traefik-gateway_1'
